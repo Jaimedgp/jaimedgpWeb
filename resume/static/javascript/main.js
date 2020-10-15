c6cc66286a44fc0a75117532a81ba5a91eda3d94
Jaimedgp <jaime.diez.gp@gmail.com>
2020-10-15 22:23:58 +0200

    Add parallax scroll animation in home page and solve section alignment. left to re-able social link

--- resume/static/javascript/main.js
+++ resume/static/javascript/main.js
@@ -1,9 +1,12 @@
 let title = document.getElementById("Title");
 let description = document.getElementById("description");
+let homeSec = document.getElementById("descriptionSec");
 
 window.addEventListener('scroll', function(){
     var value = window.scrollY;
 
-    title.style.top = -value*0.5 + 'px';
-    description.style.top = -value*0.5 + 'px';
+    title.style.top = -value*0.75 + 'px';
+    description.style.top = -value*0.75 + 'px';
+
+    homeSec.style.top = value*0.3 + 'px';
 })
