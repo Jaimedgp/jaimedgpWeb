e9af8ff952900afbcc934efe976d809de5597903
Jaimedgp <jaime.diez.gp@gmail.com>
2020-10-22 21:48:19 +0200

    Gallery section

--- resume/static/javascript/main.js
+++ resume/static/javascript/main.js
@@ -1,3 +1,6 @@
+/*
+ * Function to make scroll animation on home page
+ */
 let description = document.getElementById("title");
 let homeSec = document.getElementById("home");
 var init = screen.height*0.1;
@@ -9,3 +12,21 @@ window.addEventListener('scroll', function(){
 
     homeSec.style.top = -init + value*0.3 + 'px';
 })
+
+/*
+ * Gallery animation
+ */
+let gallery = document.getElementsByClassName("gallery");
+
+let photo1 = document.getElementById("photo1");
+photo1.onclick = function(){
+    gallery.style.left = '0';
+};
+let photo2 = document.getElementById("photo2");
+photo2.onclick = function(){
+    gallery.style.left = '-50vw';
+};
+let photo3 = document.getElementById("photo3");
+photo3.onclick = function(){
+    gallery.style.left = '-100vw';
+};
